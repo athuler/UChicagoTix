be62f19cd5bccc2b4780dfa43a19323e78638c6e
athuler <athuler@uchicago.edu>
2024-02-07 16:58:47 -0600

    Viewport Update

--- index.php
+++ index.php
@@ -35,6 +35,8 @@ preg_match_all($pattern, $file, $matches);
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body class="container">
 		<img src="" class="img-fluid" alt="" id="headerImg">
